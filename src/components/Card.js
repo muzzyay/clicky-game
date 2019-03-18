12935b379dff3acfeff31892b5dcebe865bccf39
muzzyay <muzzyay@gmail.com>
2019-03-17 22:36:25 -0700

    fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './card.css';
 
 
 
 function CardBody(props) {
-    return (<div className={`card mb-3 ${props.image===props.hover ? "shadow-lg" : " "}`} onMouseOver={()=> props.handleHover(props.image)} onMouseLeave={()=>props.mouseLeave()} onClick={()=>props.handleClick(props.image)} style={{width: "200px", height: "200px", opacity: props.hover===props.image ? 0.3 : 1}}>
+    return (<div className="card mb-3" onClick={()=>props.handleClick(props.image)} >
         <img src={props.image} className="card-img-top" alt="sightseeing"/>
     
   </div>)
--- src/components/Page.js
+++ src/components/Page.js
@@ -20,19 +20,6 @@ class Page extends React.Component {
 
     }
 
-
-    handleHover = image => {
-        this.setState({hoveredImage: image})
-    }
-
-    mouseLeave = ()=> {
-        this.setState({hoveredImage: ""})
-    }
-
-    
-
-
-
     handleClick = theImage => {
 
 
@@ -60,22 +47,21 @@ class Page extends React.Component {
     render() {
         return (<>
             <nav className="navbar navbar-white bg-white fixed-top py-4">
-               <a href="/" className="text-decoration-none"><h1 className="text-primary">Clicky Game</h1></a>
+               <a href="/" className="text-decoration-none"><h1 className="text-primary">Clicky Frenzy</h1></a>
                 {this.state.status}
 
-                <h2 className="text-primary">Score: {this.state.currentScore} |Top Score: {this.state.topScore}</h2>
+                <h2 className="text-primary">Score: {this.state.currentScore} | Top Score: {this.state.topScore} </h2>
             </nav>
             <div className="jumbotron bg-primary jumbotron-fluid mt-5">
                 <div className="container">
-                    <h1 className="display-1 text-white text-center"><strong>Clicky Game</strong></h1>
+                    <h1 className="display-1 text-white text-center"><strong>Clicky Frenzy</strong></h1>
                     <h3 className="text-center text-white">Click on an image to earn points, but don't click on any more than once!</h3>
                 </div>
             </div>
-            <div className={`container bg-dark pt-3 ${this.state.animation}`}>
+            <div className={`container bg-dark pt-3 mb-3 ${this.state.animation}`}>
                 <div className="row">
-
-                    {this.state.images.map(image => <div className="col-3" >
-                        <CardBody hover={this.state.hoveredImage} mouseLeave={this.mouseLeave} handleHover={this.handleHover} image={image} handleClick={this.handleClick} />
+                    {this.state.images.map(image => <div className="col-2 offset-md-1" >
+                        <CardBody image={image} handleClick={this.handleClick} />
                     </div>)}
                 </div>
             </div>
